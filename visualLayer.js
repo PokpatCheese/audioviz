c7e8e9245a3c56740ff0c0f4ee336e322584de91
PokpatCheese <pokpat.cheese@gmail.com>
2023-11-30 13:05:35 +0900

    複数のレイヤーを扱えるように、visualizerクラスの描画処理を拡張

---
+++ visualLayer.js
@@ -0,0 +1,40 @@
+// visualLayer.js
+
+class VisualLayer {
+    constructor(canvas, strokeStyle, offset_x, offset_y) {
+        this.canvas = canvas;
+        this.canvasContext = this.canvas.getContext('2d');
+
+        this.strokeStyle = strokeStyle;
+        this.offset_x = offset_x;
+        this.offset_y = offset_y;
+    }
+
+    draw(audioData) {
+        // 波形を描画
+        this.canvasContext.beginPath();
+        this.canvasContext.lineWidth = 2;
+        this.canvasContext.strokeStyle = this.strokeStyle;
+
+        const sliceWidth = this.canvas.width / audioData.length;
+        let x = 0;
+
+        for (let i = 0; i < audioData.length; i++) {
+            const v = audioData[i] / 128.0;
+            const y = v * this.canvas.height / 2;
+
+            if (i === 0) {
+                this.canvasContext.moveTo(x, y+this.offset_y);
+            } else {
+                this.canvasContext.lineTo(x, y+this.offset_y);
+            }
+
+            x += sliceWidth;
+        }
+
+        this.canvasContext.lineTo(this.canvas.width, this.canvas.height / 2);
+        this.canvasContext.stroke();
+    }
+}
+
+export { VisualLayer };
--- visualizer.js
+++ visualizer.js
@@ -1,38 +1,30 @@
 // visualizer.js
 
+import { VisualLayer } from "./visualLayer.js";
+
 class Visualizer {
     constructor(canvas) {
         this.canvas = canvas;
         this.canvasContext = this.canvas.getContext('2d');
+
+        // 波形を描画するレイヤーを作成する
+        this.layers = [
+            new VisualLayer(this.canvas,'green',0,0),
+            new VisualLayer(this.canvas,'rgb(0, 255, 255)',0,20),
+            new VisualLayer(this.canvas,'rgb(255, 0, 255)',0,30),
+            new VisualLayer(this.canvas,'rgb(255, 255, 0)',0,40),
+        ];
     }
 
     draw(audioData) {
         // キャンバスをクリア
         this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        // 波形を描画
-        this.canvasContext.beginPath();
-        this.canvasContext.lineWidth = 2;
-        this.canvasContext.strokeStyle = 'rgb(0, 255, 0)';
-
-        const sliceWidth = this.canvas.width / audioData.length;
-        let x = 0;
-
-        for (let i = 0; i < audioData.length; i++) {
-            const v = audioData[i] / 128.0;
-            const y = v * this.canvas.height / 2;
-
-            if (i === 0) {
-                this.canvasContext.moveTo(x, y);
-            } else {
-                this.canvasContext.lineTo(x, y);
-            }
-
-            x += sliceWidth;
-        }
-
-        this.canvasContext.lineTo(this.canvas.width, this.canvas.height / 2);
-        this.canvasContext.stroke();
+        // レイヤーを列挙
+        this.layers.forEach(function(layer){
+            // 波形をキャンバスへ描画する
+            layer.draw(audioData);
+        });
     }
 }
 
