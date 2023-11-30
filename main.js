a15d79bd43778d0ff25d5c955b8dc1c0e1718f7e
PokpatCheese <pokpat.cheese@gmail.com>
2023-11-30 12:58:21 +0900

    pauseボタンを削除

--- main.js
+++ main.js
@@ -7,20 +7,18 @@ import { Visualizer } from './visualizer.js';
 const audioProcessor = new AudioProcessor();
 const visualizer = new Visualizer(document.getElementById('visualizer'));
 
-// ユーザーがオーディオを再生したときに、オーディオデータをビジュアライザーに送信
-audioProcessor.onAudioData((audioData) => {
-    visualizer.draw(audioData);
-});
-
 // ユーザーがページを訪れたときに、オーディオの再生を開始
 window.addEventListener('load', () => {
     audioProcessor.start();
 });
 
-// 再生ボタンと一時停止ボタンの要素を取得
-const playButton = document.getElementById('play');
-const pauseButton = document.getElementById('pause');
+// ユーザーがオーディオを再生したときに、オーディオデータをビジュアライザーに送信
+audioProcessor.onAudioData((audioData) => {
+    visualizer.draw(audioData);
+});
 
+// 再生ボタンの要素を取得
+const playButton = document.getElementById('play');
 playButton.addEventListener('click', function() {
     // 音声コンテキストを再開
     audioProcessor.audioContext.resume();
